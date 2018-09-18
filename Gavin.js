81574c7c7710e4502197692a1065401fded037a8
Jay Steele <36495066+JaySteele1@users.noreply.github.com>
2018-09-17 20:45:52 -0600

    first commit

---
+++ Gavin.js
@@ -0,0 +1,113 @@
+/************************** jQuery ***************** jQuery **************************/
+
+$(document).ready(function() {
+
+    // Start of the click handlers for each picture. All in order 1-17 including all the cat Id's
+    $('#pic1').click(function() {
+        alert("Breakfast at Denny's one of Gavs favorite places to eat. Was feeling a little sick that day but was in good humor anyway. Kept his sunglasses on like a Hollywood night owl waking up at the crack of noon, wondering what t-shirt to where, playing with crayons. At least he didnt have his tablet in hand.")
+    });
+    $('#pic2').click(function() {
+        alert("Decided to take off the glasses to sniff the crayons. Certainly looks like he is enjoying it. This is typical of Gav, killing time till the food arrives. He likes the attention also. May be an only child thing.")
+    });
+    $('#pic3').click(function() {
+        alert("Finally was able to convince him to take a normal picture even though he insisted on keeping his Hollywood image going. He does take good pictures when he wants to. And ofcourse the food showed up right after this so he happy.")
+    });
+    $('#pic4').click(function() {
+        alert("Ah, the first day of fourth grade walking in with his minecraft hoody on and shorts. This is what you get when you let a 9 year old pick out his own oufit. At least he was happy and let me comb his hair for him. Went with the nice part to the side that his old man use to rock about this same age. I think he looks better though.")
+    });
+    $('#pic5').click(function() {
+        alert("In third grade they put on a play and he had to dress as a bee. His old man is afraid of bees (wasps actually) and he found it funny. I dont remember what the play was about but here he is acting typical goofy Gav getting warmed up for the show. Papa was there too.")
+    });
+    $('#pic6').click(function() {
+        alert("This one was at his friend Aiden's birthday. Trying to learn to roller-skate. felt more confident with the little walker for help. Hoping he can skate eventually. He is pretty good on skis so maybe that will transfer over one day. His old man and uncle Damon can skate so he has teachers available and his cousins Brennan, William and Aiden all play hockey so its in his blood... somewhere.")
+    });
+    $('#pic7').click(function() {
+        alert("Before we took of for Ohio to visit Yaya and Yayo Gav had to make sure his snorkel and mask fit correctly. He loves to swim but finds it necessary to wear a mask, not goggles, every time he goes. He did muster the curage at the end of the trip to jump in the pool a few times with out it on. He lived in the pool for the whole week.")
+    });
+    $('#pic8').click(function() {
+        alert("Took Gav a little to learn to ride a bike but once he did he was on it a lot. This is his bike at Nana and PapaMicks house. I managed to get him to stop for a minute to snap a pic. And ofcourse has his minecraft hoody on and the mask zipped up. Wish we had theses when i was a kid.")
+    });
+    $('#pic9').click(function() {
+        alert("As boarders and skiers one our favorite things to do is break in our new gear. Gotta get the boots broke in before the season starts so the feet dont hurt. Here is gave getting in his stance, knees bent making sure its all good and the pants fit right still.")
+    });
+    $('#pic10').click(function() {
+        alert("So Gav has been skiing for about 5 years. This was his latest gear. New helmet, ski's and boots. His mother got him some new cold gear, spider pants and jacket. Gotta try it on to make sure it fits.")
+    });
+    $('#pic11').click(function() {
+        alert("This picture is at the top of lift-2, skiers left, and the side of a snow bank. He wanted to take a pic and so we both thought to include our poweraid bottle. This was the longest my beard had been for the seaosn. Gotta keep the face warm for the high altitude runs at the top of the divide.")
+    });
+    $('#pic12').click(function() {
+        alert("The day has come! Time to get the gear on for the day. We usually get to Loveland early so we can beat traffic grab breakfast and stretch before we head to the lifts. Here's Gav being Gav again. Asked for a simple picture with his new boots and this is what we get.")
+    });
+    $('#cat1').click(function() {
+        alert("For Gavs 10th birthday we got him a new bike. A mountain bike with 6 speeds and front and rear shocks for a soft of road ride. He loved it so much we went out for a ride in cold. had to stop down at the park for a picture ofcourse. This is one of his favorite places to hang out. There are susally a few other kids here playing on the equipment.")
+    });
+    $('#cat1-5').click(function() {
+        alert("Gav running down the cat walk off lift-2 at Loveland. Says he is more comfortable with poles. Most kids his age don't use them. See the good form, legs bent, skis straight. Notices the trees to his right? Yeah, he goes through them. I have to keep him on the edge of the trees so he doesnt get too deep. And ofcourse he is not allowed near them without his helmet. He took about 10 runs this day so the last run he asked if he could wear just his beanie.")
+    });
+    $('#cat2').click(function() {
+        alert("We have several friends who board. Some of whom worked at Loveland when they moved out here. Now we are all older and have kids of our own so on some sundays we have family day. This is Gav hangin with little Acadia on her first snowboard just practicing on a little hill near the base of lift-2.")
+    });
+    $('#cat3').click(function() {
+        alert("This was a really fun day. This was Auntie Sarah's second season on skis with her husband Damon (who is an avid skier himself). They decided to come up to Loveland and ski with Gav and I. gav was so excited to show of his skills for his Auntie and Uncle. We had to capture the day with a photo.")
+    });
+    $('#cat4').click(function() {
+        alert("I thought it fitting to end this Gavin page with this pic from right after Gav's first birthday party. Had a blast, although he found out the hard way that candles are hot. I dont know what i would do without this kid in my life. Its been a real honor being his father. He did try to grab the candle and burnt his finger.")
+    });
+});
+
+
+/************************** VANILLA ***************** JS **************************/
+/*
+document.getElementById("pic1").addEventListener("click", function() {
+    alert("Breakfast at Denny's one of Gavs favorite places to eat. Was feeling a little sick that day but was in good humor anyway. Kept his sunglasses on like a Hollywood night owl waking up at the crack of noon, wondering what t-shirt to where, playing with crayons. At least he didnt have his tablet in hand.");
+});
+document.getElementById("pic2").addEventListener("click", function() {
+    alert("Decided to take off the glasses to sniff the crayons. Certainly looks like he is enjoying it. This is typical of Gav, killing time till the food arrives. He likes the attention also. May be an only child thing.");
+});
+document.getElementById("pic3").addEventListener("click", function() {
+    alert("Finally was able to convince him to take a normal picture even though he insisted on keeping his Hollywood image going. He does take good pictures when he wants to. And ofcourse the food showed up right after this so he happy.");
+});
+document.getElementById("pic4").addEventListener("click", function() {
+    alert("Ah, the first day of fourth grade walking in with his minecraft hoody on and shorts. This is what you get when you let a 9 year old pick out his own oufit. At least he was happy and let me comb his hair for him. Went with the nice part to the side that his old man use to rock about this same age. I think he looks better though.");
+});
+document.getElementById("pic5").addEventListener("click", function() {
+    alert("In third grade they put on a play and he had to dress as a bee. His old man is afraid of bees (wasps actually) and he found it funny. I dont remember what the play was about but here he is acting typical goofy Gav getting warmed up for the show. Papa was there too.");
+});
+document.getElementById("pic6").addEventListener("click", function() {
+    alert("This one was at his friend Aiden's birthday. Trying to learn to roller-skate. felt more confident with the little walker for help. Hoping he can skate eventually. He is pretty good on skis so maybe that will transfer over one day. His old man and uncle Damon can skate so he has teachers available and his cousins Brennan, William and Aiden all play hockey so its in his blood... somewhere.");
+});
+document.getElementById("pic7").addEventListener("click", function() {
+    alert("Before we took of for Ohio to visit Yaya and Yayo Gav had to make sure his snorkel and mask fit correctly. He loves to swim but finds it necessary to wear a mask, not goggles, every time he goes. He did muster the curage at the end of the trip to jump in the pool a few times with out it on. He lived in the pool for the whole week.");
+});
+document.getElementById("pic8").addEventListener("click", function() {
+    alert("Took Gav a little to learn to ride a bike but once he did he was on it a lot. This is his bike at Nana and PapaMicks house. I managed to get him to stop for a minute to snap a pic. And ofcourse has his minecraft hoody on and the mask zipped up. Wish we had theses when i was a kid.");
+});
+document.getElementById("pic9").addEventListener("click", function() {
+    alert("As boarders and skiers one our favorite things to do is break in our new gear. Gotta get the boots broke in before the season starts so the feet dont hurt. Here is gave getting in his stance, knees bent making sure its all good and the pants fit right still.");
+});
+document.getElementById("pic10").addEventListener("click", function() {
+    alert("So Gav has been skiing for about 5 years. This was his latest gear. New helmet, ski's and boots. His mother got him some new cold gear, spider pants and jacket. Gotta try it on to make sure it fits.");
+});
+document.getElementById("pic11").addEventListener("click", function() {
+    alert("This picture is at the top of lift-2, skiers left, and the side of a snow bank. He wanted to take a pic and so we both thought to include our poweraid bottle. This was the longest my beard had been for the seaosn. Gotta keep the face warm for the high altitude runs at the top of the divide.");
+});
+document.getElementById("pic12").addEventListener("click", function() {
+    alert("The day has come! Time to get the gear on for the day. We usually get to Loveland early so we can beat traffic grab breakfast and stretch before we head to the lifts. Here's Gav being Gav again. Asked for a simple picture with his new boots and this is what we get.");
+});
+document.getElementById("cat1").addEventListener("click", function() {
+    alert("For Gavs 10th birthday we got him a new bike. A mountain bike with 6 speeds and front and rear shocks for a soft of road ride. He loved it so much we went out for a ride in cold. had to stop down at the park for a picture ofcourse. This is one of his favorite places to hang out. There are susally a few other kids here playing on the equipment.");
+});
+document.getElementById("cat1-5").addEventListener("click", function() {
+    alert("Gav running down the cat walk off lift-2 at Loveland. Says he is more comfortable with poles. Most kids his age don't use them. See the good form, legs bent, skis straight. Notices the trees to his right? Yeah, he goes through them. I have to keep him on the edge of the trees so he doesnt get too deep. And ofcourse he is not allowed near them without his helmet. He took about 10 runs this day so the last run he asked if he could wear just his beanie.");
+});
+document.getElementById("cat2").addEventListener("click", function() {
+    alert("We have several friends who board. Some of whom worked at Loveland when they moved out here. Now we are all older and have kids of our own so on some sundays we have family day. This is Gav hangin with little Acadia on her first snowboard just practicing on a little hill near the base of lift-2.");
+});
+document.getElementById("cat3").addEventListener("click", function() {
+    alert("This was a really fun day. This was Auntie Sarah's second season on skis with her husband Damon (who is an avid skier himself). They decided to come up to Loveland and ski with Gav and I. gav was so excited to show of his skills for his Auntie and Uncle. We had to capture the day with a photo.");
+});
+document.getElementById("cat4").addEventListener("click", function() {
+    alert("I thought it fitting to end this Gavin page with this pic from right after Gav's first birthday party. Had a blast, although he found out the hard way that candles are hot. I dont know what i would do without this kid in my life. Its been a real honor being his father. He did try to grab the candle and burnt his finger.");
+});
+*/
